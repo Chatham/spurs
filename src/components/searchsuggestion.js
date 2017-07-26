73b43f8c5eafdb97f719b6bdbbf49f799880b3c7
Jon Carl <jcarl@chathamfinancial.com>
2017-07-26 10:59:40 -0400

    fix bug where suggestion highlighting was case sensative

--- src/components/searchsuggestion.js
+++ src/components/searchsuggestion.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchSuggestion = (suggestion, { query }) => {
-  const sSplit = suggestion.split(query);
+  const queryReg = new RegExp(query, "i");
+  const sSplit = suggestion.split(queryReg);
   const beginning = sSplit[0] || "";
-  const highlight = query;
+  const highlight = suggestion.match(queryReg)[0] || "";
   const end = sSplit[1] || "";
+
+  let suggestionEl;
+  if (sSplit.length > 1) {
+    suggestionEl = <p className="suggestion">{beginning}<strong>{highlight}</strong>{end}</p>;
+  } else {
+    suggestionEl = <p className="suggestion">{suggestion}</p>;
+  }
 
   return (
     <div className="searchsuggestion_container">
-      <p className="suggestion">{beginning}<strong>{highlight}</strong>{end}</p>
+      {suggestionEl}
     </div>
   )
 }
